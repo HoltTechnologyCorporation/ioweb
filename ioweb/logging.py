05b0c9549a353230bd81d066d69fa40f436ab488
Gregory Petukhov <lorien@lorien.name>
2020-01-01 13:33:11 +0300

    Rename ioweb.logging:setup_fail_logger

--- ioweb/logging.py
+++ ioweb/logging.py
@@ -2,7 +2,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-def setup_fail_logger(
+def setup_fail_logging(
         filename, mode='a', max_bytes=(50 * 1024 * 1024),
         formatter='%(asctime)s [%(levelname)s] (%(name)s) %(message)s',
     ):
