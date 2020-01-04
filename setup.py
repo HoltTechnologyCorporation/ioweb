2da789fb6f88372814f11892d1b80f9f241ac0ec
Gregory Petukhov <lorien@lorien.name>
2020-01-05 00:17:32 +0300

    Bump version: 0.0.19 → 0.0.20

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as inp:
 
 setup(
     name = 'ioweb',
-    version = '0.0.19',
+    version = '0.0.20',
     author = 'Gregory Petukhov',
     author_email = 'lorien@lorien.name',
     maintainer='Gregory Petukhov',
